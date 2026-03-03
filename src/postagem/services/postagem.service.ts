23b69dee5d86599d2f1610caac8b3d8d3b44b271
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-03-03 13:07:13 -0300

    ✨ New Feature: Construção do Recurso Postagem

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Postagem } from "../entities/postagem.entity";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 
 @Injectable()
 export class PostagemService{
@@ -12,7 +12,59 @@ export class PostagemService{
   ){}
 
   async findAll(): Promise<Postagem[]>{
-    // SELECT * FROM tb_postagens
+    // SELECT * FROM tb_postagens;
     return this.postagemRepository.find();
   }
+
+  async findById(id: number): Promise<Postagem>{
+    // SELECT * FROM tb_postagens WHERE id = ?;
+    const postagem = await this.postagemRepository.findOne({
+      where:{
+        id
+      }
+    })
+
+    if (!postagem)
+      throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
+
+    return postagem;
+  }
+
+  async findAllByTitulo(titulo: string): Promise<Postagem[]>{
+    // SELECT * FROM tb_postagens WHERE titulo LIKE '%?%';
+    return this.postagemRepository.find({
+      where:{
+        titulo: ILike(`%${titulo}%`)
+      }
+    })
+  }
+
+  async create(postagem: Postagem): Promise<Postagem>{
+    // INSERT INTO tb_postagens (titulo, texto) VALUES (?, ?);
+    return this.postagemRepository.save(postagem);
+  }
+
+  async update(postagem: Postagem): Promise<Postagem>{
+
+    if (!postagem.id || postagem.id <= 0)
+      throw new HttpException("O ID da postagem é inválido!", HttpStatus.BAD_REQUEST);
+
+    await this.findById(postagem.id);
+
+    // UPDATE tb_postagens SET titulo = ?, 
+    // texto = ? , 
+    // data = CURRENT_TIMESTAMP()
+    // WHERE id = ?;
+    return this.postagemRepository.save(postagem);
+  }
+
+  async delete(id: number): Promise<DeleteResult>{
+    
+    await this.findById(id);
+
+    // DELETE tb_postagens FROM id = ?;
+    return this.postagemRepository.delete(id);
+    
+  }
+
 }
